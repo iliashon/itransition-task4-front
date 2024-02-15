60f07ab6806c58a25ad49fa9691de93630443f76
Tupalski Ilya <itupalski@gmail.com>
2024-02-16 00:35:55 +0400

    fix: rename title sign-up page (Fri Feb 16 00:35:55 +04 2024)

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -54,7 +54,7 @@ const SignUp = () => {
             </div>
             <div className="mx-auto max-w-2xl text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    Registartion
+                    Registration
                 </h2>
                 {error ? (
                     <Alert
